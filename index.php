0979b001e05e856dc913a127a9a851351ae70dc3
ashwini0529 <ashwinipurohit@yahoo.in>
2015-05-02 15:48:35 +0530

    updated Index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <html>
 <body>
-	<h4>ACM Member Login</h4>
+	<h2>ACM Member Login</h2>
   <form action="form_login.php" method="GET">
 
   Name: <input type="text" name="name" />
@@ -10,21 +10,21 @@
   <input type="submit" />
   </form>
 
- <h4>ACM Member Register!</h4>
+ <h2>ACM Member Register!</h2>
  <form action="form_register.php" method="GET">
 
-  Name:&nbsp; &nbsp;<input type="text" name="name" /><br>
-  Password:&nbsp;&nbsp;&nbsp;&nbsp; <input type="password" name="pass" /><br>
-  Reg No: <input type ="text" name="regno"><br>
-  Category&nbsp;&nbsp;<select name ="category">
+  Name:&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<input type="text" name="name" /><br><br>
+  Password:&nbsp;&nbsp;&nbsp;&nbsp; <input type="password" name="pass" /><br><br>
+  Reg No: &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;<input type ="text" name="regno"><br><br>
+  Category&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;<select name ="category">
   	<option value="Technical" >Technical</option>
  	<option value="Management">Management</option>
   </select>
-  <br>
+  <br><br>
   Gender
   <input type="radio" name="gender" value="MALE">Male</input>
   <input type="radio" name="gender" value="FEMALE">Female</input>
-<br>
+<br><br>
 
 
 
